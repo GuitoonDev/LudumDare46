b2072fe3a97009a08d81507ee3f740194210825b
Sorangon <julien.delaunay034@gmail.com>
2020-04-17 23:20:39 +0200

    Moved godot cpp project file

---
+++ Project/Src/LudumDare46/PlayerTest.cpp
@@ -0,0 +1,25 @@
+#include "PlayerTest.h"
+
+
+using namespace godot;
+
+void PlayerTest::_register_methods() 
+{
+}
+
+void PlayerTest::_init()
+{
+}
+
+void PlayerTest::_process(float delta)
+{
+}
+
+PlayerTest::PlayerTest()
+{
+}
+
+
+PlayerTest::~PlayerTest()
+{
+}
