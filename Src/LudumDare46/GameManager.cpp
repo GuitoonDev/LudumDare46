e5f1967ad38ab2725e2a9cffe6ff41f3b814f21b
JulienDelaunay <julien.delaunay034@gmail.com>
2020-04-18 18:34:32 +0200

    Created Game Manager source files and prototype methods

--- Src/LudumDare46/GameManager.cpp
+++ Src/LudumDare46/GameManager.cpp
@@ -1,10 +1,16 @@
 #include "GameManager.h"
 
 using namespace godot;
+<<<<<<< HEAD
 using namespace std;
 
 #pragma region Construction
 GameManager::GameManager():m_score(0), m_label(nullptr){}
+=======
+
+#pragma region Construction
+GameManager::GameManager(){}
+>>>>>>> Created Game Manager source files and prototype methods
 GameManager::~GameManager(){}
 #pragma endregion
 
@@ -12,6 +18,7 @@ GameManager::~GameManager(){}
 #pragma region Godot Methods
 void GameManager::_register_methods()
 {
+<<<<<<< HEAD
 	register_method((char*)"_process", &GameManager::_process);
 	register_signal<GameManager>((char *)"game_started", "owner", GODOT_VARIANT_TYPE_OBJECT);
 	register_signal<GameManager>((char *)"game_over", "owner", GODOT_VARIANT_TYPE_OBJECT);
@@ -28,10 +35,13 @@ void GameManager::_process()
 	if (_input->is_action_just_pressed("ui_cancel")) {
 		ReloadGame();
 	}
+=======
+>>>>>>> Created Game Manager source files and prototype methods
 }
 
 void GameManager::_init()
 {
+<<<<<<< HEAD
 	NodePath path = get_path();
 	Godot::print(path);
 	Node* _labelNode = get_node(NodePath("Control/ScoreDisplay"));
@@ -49,28 +59,46 @@ void GameManager::_init()
 void GameManager::AddPoints(const int points)
 {
 	m_score += points;
+=======
+	Godot::print("I work !");
+}
+#pragma endregion
+
+#pragma region Game Management
+void GameManager::AddPoints()
+{
+>>>>>>> Created Game Manager source files and prototype methods
 }
 
 //The start function of the game
 void GameManager::StartGame()
 {
+<<<<<<< HEAD
 	Godot::print("Start game");
 	emit_signal("game_started", this);
+=======
+>>>>>>> Created Game Manager source files and prototype methods
 }
 
 void GameManager::Lose()
 {
+<<<<<<< HEAD
 	emit_signal("game_over", this);
 	Godot::print("Reload");
+=======
+>>>>>>> Created Game Manager source files and prototype methods
 }
 
 void GameManager::Pause()
 {
+<<<<<<< HEAD
 	Godot::print("TODO : Pause the game");
 }
 
 void GameManager::ReloadGame()
 {
 	get_tree()->reload_current_scene();
+=======
+>>>>>>> Created Game Manager source files and prototype methods
 }
 #pragma endregion
--- Src/LudumDare46/GameManager.h
+++ Src/LudumDare46/GameManager.h
@@ -2,6 +2,7 @@
 
 #include <core/Godot.hpp>
 #include <Node.hpp>
+<<<<<<< HEAD
 #include <core/NodePath.hpp>
 #include <core/String.hpp>
 #include <Input.hpp>
@@ -10,11 +11,15 @@
 #include <iostream>
 #include <Curve.hpp>
 #include <Label.hpp>
+=======
+#include <core/String.hpp>
+>>>>>>> Created Game Manager source files and prototype methods
 
 namespace godot {
 	class GameManager : public Node {
 		GODOT_CLASS(GameManager, Node)
 	public:
+<<<<<<< HEAD
 		GameManager();
 		~GameManager();
 
@@ -31,5 +36,25 @@ namespace godot {
 		Label* m_label;
 
 		void ReloadGame();
+=======
+#pragma region Construction
+		GameManager();
+		~GameManager();
+#pragma endregion
+
+#pragma region Godot Methods
+		static void _register_methods();
+		void _init();
+#pragma endregion
+
+#pragma region Game Management
+		void AddPoints();
+		void StartGame();
+		void Lose();
+		void Pause();
+#pragma endregion
+	private:
+		float m_score;
+>>>>>>> Created Game Manager source files and prototype methods
 	};
 }
