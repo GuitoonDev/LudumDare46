196c1ef5cafd822cdbedc57bc91d172d7a5d95db
JulienDelaunay <julien.delaunay034@gmail.com>
2020-04-18 17:32:54 +0200

    Reinitialize project

--- Src/LudumDare46/PlayerTest.cpp
+++ Src/LudumDare46/PlayerTest.cpp
@@ -2,9 +2,10 @@
 
 using namespace godot;
 
-void PlayerTest::_register_methods() 
+void PlayerTest::_register_methods()
 {
 	register_method((char*)"_process", &PlayerTest::_process);
+	register_property<PlayerTest, float>("Speed", &PlayerTest::speed, 10.0);
 }
 
 void PlayerTest::_init()
@@ -14,15 +15,22 @@ void PlayerTest::_init()
 
 void PlayerTest::_process(float delta)
 {
-	Godot::print(String("Pouette"));
-	motion = Vector2(2000.0, 0.0);
-	set_position(get_position() + Vector2(1.0, 0.0));
+	Input* input = Input::get_singleton();
+
+	if (input->is_action_pressed("ui_accept")) {
+		Godot::print(String("Pouette"));
+		motion = Vector2(2000.0, 0.0);
+		set_position(get_position() + Vector2(1.0, 0.0));
+	}
+
 
 	//move_and_slide(motion);
 }
 
-PlayerTest::PlayerTest() : motion(Vector2(0,0))
+PlayerTest::PlayerTest()
 {
+	motion = Vector2(0, 0);
+	speed = 10.0;
 }
 
 PlayerTest::~PlayerTest()
--- Src/LudumDare46/PlayerTest.h
+++ Src/LudumDare46/PlayerTest.h
@@ -3,12 +3,14 @@
 #include <core/Godot.hpp>
 #include <core/String.hpp>
 #include <KinematicBody2D.hpp>
+#include <Input.hpp>
 
 namespace godot {
 	class PlayerTest : public KinematicBody2D{
 		GODOT_CLASS(PlayerTest, KinematicBody2D)
 	private:
 		Vector2 motion;
+		float speed;
 
 	public:
 		static void _register_methods();
