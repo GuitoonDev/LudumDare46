570b4e4e8dbc534196e6b921b1c16803fe425c6b
JulienDelaunay <julien.delaunay034@gmail.com>
2020-04-19 00:07:46 +0200

    Merge from dev

--- Src/LudumDare46/GodotLibrary.cpp
+++ Src/LudumDare46/GodotLibrary.cpp
@@ -1,6 +1,7 @@
 #include "PlayerTest.h"
 #include "PlayerController.h"
 #include "Planet.h"
+#include "GameManager.h"
 
 using namespace godot;
 
@@ -18,4 +19,5 @@ extern "C" void GDN_EXPORT godot_nativescript_init(void* handle) {
 
 	register_class<PlayerController>();
 	register_class<Planet>();
+	register_class<GameManager>();
 }
