3985ea6e800a8599ef63addbbdb65689b4ae0a80
JulienDelaunay <julien.delaunay034@gmail.com>
2020-04-18 20:23:42 +0200

    Add game state callbacks signals to the Game Manager

---
+++ Project/GameContent/Gameplay/GameManager/Node.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func _on_GameManager_game_started(owner):
+	print("game started with ")
+	pass # Replace with function body.
--- Src/LudumDare46/GameManager.cpp
+++ Src/LudumDare46/GameManager.cpp
@@ -1,9 +1,10 @@
 #include "GameManager.h"
 
 using namespace godot;
+using namespace std;
 
 #pragma region Construction
-GameManager::GameManager(){}
+GameManager::GameManager() :m_score(0){}
 GameManager::~GameManager(){}
 #pragma endregion
 
@@ -11,29 +12,54 @@ GameManager::~GameManager(){}
 #pragma region Godot Methods
 void GameManager::_register_methods()
 {
+	register_method((char*)"_process", &GameManager::_process);
+	register_signal<GameManager>((char *)"game_started", "owner", GODOT_VARIANT_TYPE_OBJECT);
+	register_signal<GameManager>((char *)"game_over", "owner", GODOT_VARIANT_TYPE_OBJECT);
+}
+
+void GameManager::_process()
+{
+	Input* _input = Input::get_singleton();
+
+	if (_input->is_action_just_pressed("ui_accept")) {
+		StartGame();
+	}
+
+	if (_input->is_action_just_pressed("ui_cancel")) {
+		ReloadGame();
+	}
 }
 
 void GameManager::_init()
 {
-	Godot::print("I work !");
 }
 #pragma endregion
 
 #pragma region Game Management
-void GameManager::AddPoints()
+void GameManager::AddPoints(const int points)
 {
+	m_score += points;
 }
 
 //The start function of the game
 void GameManager::StartGame()
 {
+	Godot::print("Start game");
+	emit_signal("game_started", this);
 }
 
 void GameManager::Lose()
 {
+	emit_signal("game_over", this);
+	Godot::print("Reload");
 }
 
 void GameManager::Pause()
 {
+	Godot::print("TODO : Pause the game");
+}
+void GameManager::ReloadGame()
+{
+	get_tree()->reload_current_scene();
 }
 #pragma endregion
