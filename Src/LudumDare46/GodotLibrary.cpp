cb30081d3b6aea0adcf241262094291bc41e8ae7
JulienDelaunay <julien.delaunay034@gmail.com>
2020-04-19 12:13:12 +0200

    Added score display

--- Src/LudumDare46/GodotLibrary.cpp
+++ Src/LudumDare46/GodotLibrary.cpp
@@ -1,5 +1,8 @@
 #include "PlayerTest.h"
 #include "GameManager.h"
+#include "Meteorite.h"
+#include "Planet.h"
+#include "PlayerController.h"
 
 using namespace godot;
 
@@ -14,5 +17,8 @@ extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_opt
 extern "C" void GDN_EXPORT godot_nativescript_init(void* handle) {
 	Godot::nativescript_init(handle);
 	register_class<PlayerTest>();
+	register_class<Meteorite>();
+	register_class<Planet>();
 	register_class<GameManager>();
+	register_class<PlayerController>();
 }
