e9aa71c23356ad4644d27fb1f75bd30ed6aa72b1
JulienDelaunay <julien.delaunay034@gmail.com>
2020-04-19 14:08:01 +0200

    Created end menu and game state logic

--- Src/LudumDare46/GameManager.cpp
+++ Src/LudumDare46/GameManager.cpp
@@ -4,7 +4,13 @@ using namespace godot;
 using namespace std;
 
 #pragma region Construction
-GameManager::GameManager() :m_score(0), m_label(nullptr) {}
+GameManager::GameManager() :
+m_score(0), 
+m_gameState(Idle),
+m_scoreText(nullptr), 
+m_titleScreen(nullptr),
+m_defeatScreen(nullptr){}
+
 GameManager::~GameManager() {}
 #pragma endregion
 
@@ -19,23 +25,54 @@ void GameManager::_register_methods()
 
 void GameManager::_ready()
 {
-	m_label = cast_to<Label>(get_node("UI/HUD/Score/ScoreDisplay"));
+	m_scoreText = cast_to<Label>(get_node("UI/HUD/Score/ScoreDisplay"));
 	m_titleScreen = cast_to<Control>(get_node("UI/Menu"));
-	m_label->set_text(to_string(m_score).c_str());
+	m_defeatScreen = cast_to<Control>(get_node("UI/GameOverScreen"));
+
+	//Init score text
+	m_scoreText->set_text(to_string(m_score).c_str());
+
+	//Set idle game state
+	m_gameState = Idle;
 
 	DisplayTitleScreen(true);
+	DisplayGameOverScreen(false);
 }
 
 void GameManager::_process()
 {
 	Input* _input = Input::get_singleton();
 
-	if (_input->is_action_just_pressed("ui_accept")) {
-		StartGame();
-	}
+	switch (m_gameState)
+	{
+	case Idle:
+		if (_input->is_action_just_pressed("ui_accept")) {
+			StartGame();
+		}
 
-	if (_input->is_action_just_pressed("ui_cancel")) {
-		ReloadGame();
+		if (_input->is_action_just_pressed("ui_cancel")) {
+			LeaveGame();
+		}
+		break;
+
+	case Playing:
+		if (_input->is_action_just_pressed("ui_accept")) {
+			Lose();
+		}
+		break;
+
+	case Over:
+		if (_input->is_action_just_pressed("ui_accept")) {
+			ReloadGame();
+		}
+
+		if (_input->is_action_just_pressed("ui_cancel")) {
+			LeaveGame();
+		}
+		break;
+
+	default:
+		break;
 	}
 }
 
@@ -43,24 +80,24 @@ void GameManager::_init() {}
 #pragma endregion
 
 #pragma region Game Management
-void GameManager::AddPoints(const int points)
+void GameManager::AddPoints(const int p_points)
 {
-	m_score += points;
-	m_label->set_text(to_string(m_score).c_str());
+	m_score += p_points;
+	m_scoreText->set_text(to_string(m_score).c_str());
 }
 
-//The start function of the game
 void GameManager::StartGame()
 {
 	Godot::print("Start game");
 	DisplayTitleScreen(false);
+	m_gameState = Playing;
 	emit_signal("game_started", this);
 }
 
 void GameManager::Lose()
 {
+	m_gameState = Over;
 	emit_signal("game_over", this);
-	Godot::print("Reload");
 }
 
 void GameManager::Pause()
@@ -72,12 +109,30 @@ void GameManager::ReloadGame()
 {
 	get_tree()->reload_current_scene();
 }
-void GameManager::DisplayTitleScreen(const bool display)
+
+
+void GameManager::DisplayGameOverScreen(const bool p_display)
 {
-	if (display == true) {
+	if (p_display) {
+		m_defeatScreen->show();
+
+	} else {
+		m_defeatScreen->hide();
+	}
+}
+
+void GameManager::DisplayTitleScreen(const bool p_display)
+{
+	if (p_display) {
 		m_titleScreen->show();
 	} else {
 		m_titleScreen->hide();
 	}
 }
+
+void GameManager::LeaveGame()
+{
+	get_tree()->quit();
+}
+
 #pragma endregion
